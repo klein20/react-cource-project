05e23b0ecbeecbbba84e1aca2fda79ebbb7560b6
klein20 <olexandrkleyn@gmail.com>
2020-01-30 11:58:32 +0200

    added cockpit component and made project structure better

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 
 class App extends Component {
@@ -47,42 +47,20 @@ class App extends Component {
 
   render () {
     let persons = null;
-    let btnClass = '';
 
     if ( this.state.showPersons ) {
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <ErrorBoundary key={person.id}> 
-              <Person
-              click={() => this.deletePersonHandler(index)}
-              name={person.name} 
-              age={person.age}
-              changed={(event) => this.nameChangedHandler(event, person.id)} /> 
-            </ErrorBoundary>
-          })}
-        </div>
-      );
-
-      btnClass = classes.Red;
+      persons = <Persons 
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangedHandler}  />;
     }
- 
-
-    const assignedclasses = [];
-    if (this.state.persons.length <= 2) {
-      assignedclasses.push(classes.red);
-    }
-    if (this.state.persons.length <= 1) {
-      assignedclasses.push(classes.bold);
-    }
-    
+   
     return (
       <div className={classes.App}>
-        <h1>Hi, I'm a React App</h1>
-        <p className={assignedclasses.join(' ')}>This is really working!</p>
-        <button 
-          className={btnClass}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        <Cockpit 
+        showPersons={this.state.showPersons}
+        persons={this.state.persons}
+        clicked={this.togglePersonsHandler} />
         {persons}
       </div>
     );
